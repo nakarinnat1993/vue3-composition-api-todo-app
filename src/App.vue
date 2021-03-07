e6642a0309475f204d41f1a72aa769de6eb5728b
unknown <nakarinnat1993@gmail.com>
2021-03-07 12:26:35 +0700

    vue3-composition-api-todo-app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Vue 3 Composition API TODO APP</h1>
+  <form @submit.prevent="addTodo">
+    <label for="newTodo">TODO : </label>
+    <input type="text" name="newTodo" v-model="newTodo" />
+    <button>Add</button>
+  </form>
+  <!-- <h2>{{ newTodo }}</h2> -->
+  <button @click="removeAll()">Remove All</button>
+  <button @click="markAllDone()">All Done</button>
+  <center>
+    <table class="center">
+      <tr v-for="(todo, index) in todos" :key="todo.in" class="todo">
+        <td><h4 :class="{ done : todo.done }" @click="toggleDone(todo)">{{ todo.content }} </h4></td>
+        <td><button @click="removeTodo(index)">x</button></td>
+      </tr>
+    </table>
+  </center>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const newTodo = ref("");
+    const todos = ref([]);
+
+    function addTodo() {
+      todos.value.push({
+        id: Date.now(),
+        done: false,
+        content: newTodo.value,
+      });
+      newTodo.value = "";
+    }
+
+    function toggleDone(todo){
+      todo.done = !todo.done;
+    }
+    function removeTodo(index){
+      if(confirm(`Remove ${index} ?`)){
+        todos.value.splice(index, 1);
+      }
+    }
+
+    function markAllDone(){
+      todos.value.forEach((todo)=>todo.done=true)
+    }
+
+    function removeAll(){
+      if(confirm("Remove ALL ?")){
+        todos.value=[];
+      }
+    }
+    return { newTodo, addTodo, todos, toggleDone, removeTodo, markAllDone, removeAll };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +66,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.done{
+  text-decoration: line-through;
+}
+.todo{
+  cursor: pointer;
 }
 </style>
